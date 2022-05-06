a339c77495bab153357086a40ed83697d4d7e0b3
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2022-05-06 23:52:45 +0200

    Handling 0 / feedback in template literals for valid nums

--- task1/task1.js
+++ task1/task1.js
@@ -13,17 +13,23 @@ let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
 function combinator(n, arr) {
-  if (n > 0) {
-    for (let i = 0; i < n; i++) {
-      arr.unshift(arr.pop());
+  if (n !== 0) {
+    if (n > 0) {
+      for (let i = 0; i < n; i++) {
+        arr.unshift(arr.pop());
+      }
+      console.log(`Array elements moved to the right for ${n} places. Here is your new array:`, arr);
+    } else if (n < 0) {
+      let absNum = Math.abs(n);
+      for (let i = 0; i < absNum; i++) {
+        arr.push(arr.shift());
+      }
+      console.log(`Array elements moved to the left for ${absNum} places. Here is your new array:`, arr);
     }
-  } else if (n < 0) {
-    let absNum = Math.abs(n);
-    for (let i = 0; i < absNum; i++) {
-      arr.push(arr.shift());
-    }
-  } console.log(arr);
+  } else {
+    console.log("You've entered 0. Please enter a valid number!")
+  }
 }
 
-combinator(-2, nums)
+combinator(-4, nums)
 
